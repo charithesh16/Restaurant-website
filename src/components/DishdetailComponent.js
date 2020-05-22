ece452fb7fdb58a8272cd196280aaca5847f1ed0
charithesh16 <charithesh16@gmail.com>
2020-05-22 21:57:20 +0530

    Presentational and Cantainer Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class DishDetail extends Component {
                         return (
                             <React.Fragment>
                                 <li> {comment.comment}</li>
-                                <li>--{comment.author},{comment.date}</li>
+                                <li>--{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
                             </React.Fragment>
                         );
                     })}
@@ -39,12 +39,14 @@ class DishDetail extends Component {
         console.log(this.props.dish);
         if (this.props.dish != null)
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className='col-12 col-md-5 m-1'>
-                        {this.renderComments(this.props.dish.comments)}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className='col-12 col-md-5 m-1'>
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
